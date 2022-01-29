c1b5525ea5390eff87250b4944e2d3c754f1c608
AliceNov <alisa.nov72@gmail.com>
2022-01-29 16:55:10 +0300

    authenticaton

---
+++ api/src/user/user.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserController } from './controller/user.controller';
+import { UserEntity } from './model/user.entity';
+import { UserService } from './service/user.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthModule
+  ],
+  providers: [UserService],
+  controllers: [UserController]
+})
+export class UserModule {}
