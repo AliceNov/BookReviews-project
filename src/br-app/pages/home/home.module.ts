d1dc6c1836093ec8bc7164db18d9c61c88d66795
AliceNov <alisa.nov72@gmail.com>
2022-02-15 17:35:06 +0300

    add card component

--- src/br-app/pages/home/home.module.ts
+++ src/br-app/pages/home/home.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from "@angular/common";
 
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home/home.component";
+import { CardComponent } from "./card/card.component";
+import { MaterialModule } from "src/br-app/modules/material/material.module";
 
 
 @NgModule({
   declarations: [
+    CardComponent,
     HomeComponent,
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
+    MaterialModule,
   ]
 })
 export class HomeModule { }
