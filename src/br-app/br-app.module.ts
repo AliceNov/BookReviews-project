00871207b55e5a181d6100ff3f15b345686145b8
AliceNov <alisa.nov72@gmail.com>
2022-02-15 17:43:20 +0300

    add signup

--- src/br-app/br-app.module.ts
+++ src/br-app/br-app.module.ts
@@ -12,6 +12,14 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { LoginModule } from "./pages/login/login.module";
 import { HomeModule } from "./pages/home/home.module";
+import { StoreModule } from "@ngrx/store";
+import { reducers } from "./store";
+import { ReviewEffects } from "./store/effects/review.effect";
+import { BookEffects } from "./store/effects/book.effect";
+import { UserEffects } from "./store/effects/user.effect";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
+import { AuthEffects } from "./store/effects/auth.effect";
 
 @NgModule({
     declarations: [
@@ -31,6 +39,10 @@ import { HomeModule } from "./pages/home/home.module";
         LoginModule,
 
         MaterialModule,
+
+        StoreModule.forRoot(reducers),
+        EffectsModule.forRoot([AuthEffects, BookEffects, ReviewEffects, UserEffects]),
+        StoreRouterConnectingModule.forRoot(),
     ],
     providers: [],
     bootstrap: [BrAppComponent],
--- src/br-app/pages/login/components/login.component.ts
+++ src/br-app/pages/login/components/login.component.ts
@@ -1,7 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { signUp } from "src/br-app/store/actions/auth.action";
 import { passwordValidators } from "src/br-app/validators/password.validator";
 import { validation } from "src/br-app/validators/patterns-validators.validator";
+import { User } from "src/models/user.model";
 import { FormFieldValid } from "../../../validators/form-field-valide";
 
 @Component({
@@ -15,10 +19,15 @@ export class LoginComponent implements OnInit {
   public signInForm: FormGroup;
   public signUpForm: FormGroup;
 
+  public signUpUser: User;
+
   public validMessage = validation.errorMessage;
 
   constructor(private fb: FormBuilder,
-              public formFieldValid: FormFieldValid) { }
+              public formFieldValid: FormFieldValid,
+              private store: Store,
+              private router: Router,
+              private cf: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.createSignInForm();
@@ -75,7 +84,17 @@ export class LoginComponent implements OnInit {
   }
 
   signUp(): void {
-
+    if (this.signUpForm.valid) {
+      this.signUpUser = {
+        name: this.signUpForm.get("name").value,
+        username: this.signUpForm.get("username").value,
+        email: this.signUpForm.get("email").value,
+        password: this.signUpForm.get("password").value
+      };
+      this.store.dispatch(signUp({ user: this.signUpUser }));
+      this.router.navigate(["/home"]);
+      this.cf.detectChanges();
+    }
   }
 
 }
